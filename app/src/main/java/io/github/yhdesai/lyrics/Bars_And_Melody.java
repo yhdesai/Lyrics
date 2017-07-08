7ede9cc88cd89dcb353bdcc3a3a0102844079f00
Yash <yhdelectric@gmail.com>
2017-07-08 15:09:06 +0530

    still playing with arrays, made a fixed size array in this commit

--- app/src/main/java/io/github/yhdesai/lyrics/Bars_And_Melody.java
+++ app/src/main/java/io/github/yhdesai/lyrics/Bars_And_Melody.java
@@ -3,18 +3,13 @@ package io.github.yhdesai.lyrics;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 
 
 
-int [] bamSongs = new int[5];
 
-bamSongs[0] = 1;
-bamSongs[1] = 2;
-bamSongs[2] = 3;
-bamSongs[3] = 4;
-bamSongs[4] = 5;
 
 public class Bars_And_Melody extends AppCompatActivity {
 
@@ -22,6 +17,17 @@ public class Bars_And_Melody extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bars__and__melody);
+
+        String [] bamSongs = new String[5];
+
+        bamSongs[0] ="7 years";
+        bamSongs[1] = "Battle Scars";
+        bamSongs[2] = "Beautiful";
+        bamSongs[3] = "Closer";
+        bamSongs[4] = "Dont let me down";
+
+
+        Log.e("Bars_And_Melody","Word at index 0: " + bamSongs[0]);
     }
 
 
