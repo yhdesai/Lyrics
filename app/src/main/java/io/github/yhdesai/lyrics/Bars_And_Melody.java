3b793676bb2da07259c9f24fd80b3715df403c24
Yash <yhdelectric@gmail.com>
2017-07-08 15:59:28 +0530

    added while loop in array to create button until it reaches array size

--- app/src/main/java/io/github/yhdesai/lyrics/Bars_And_Melody.java
+++ app/src/main/java/io/github/yhdesai/lyrics/Bars_And_Melody.java
@@ -1,14 +1,10 @@
 package io.github.yhdesai.lyrics;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import java.util.ArrayList;
 
 
@@ -23,84 +19,26 @@ public class Bars_And_Melody extends AppCompatActivity {
 
 
         bamSongs.add("1");
+        bamSongs.add("2");
 
 
-        LinearLayout bamSongsActivity=(LinearLayout)findViewById(R.id.bamSongsActivity);
-
+        LinearLayout bamSongsActivity = (LinearLayout) findViewById(R.id.bamSongsActivity);
         int index = 0;
 
-        Button bamSongButtons1 = new Button(this);
-        bamSongButtons1.setText(bamSongs.get(index));
-        bamSongsActivity.addView(bamSongButtons1);
+        while (index < bamSongs.size()) {
 
-        index = index + 1;
-
-        Button bamSongButtons2 = new Button(this);
-        bamSongButtons2.setText(bamSongs.get(index));
-        bamSongsActivity.addView(bamSongButtons2);
-
-        index = index + 1;
-
-        Button bamSongButtons3 = new Button(this);
-        bamSongButtons3.setText(bamSongs.get(index));
-        bamSongsActivity.addView(bamSongButtons3);
-
-        index = index + 1;
-
-        Button bamSongButtons4 = new Button(this);
-        bamSongButtons4.setText(bamSongs.get(index));
-        bamSongsActivity.addView(bamSongButtons4);
-
-        index = index + 1;
-
-        Button bamSongButtons5 = new Button(this);
-        bamSongButtons5.setText(bamSongs.get(index));
-        bamSongsActivity.addView(bamSongButtons5);
-
-        index = index + 1;
-
-        Button bamSongButtons6 = new Button(this);
-        bamSongButtons6.setText(bamSongs.get(index));
-        bamSongsActivity.addView(bamSongButtons6);
-
-        index = index + 1;
-
-        Button bamSongButtons7 = new Button(this);
-        bamSongButtons7.setText(bamSongs.get(index));
-        bamSongsActivity.addView(bamSongButtons7);
+            Button bamSongButtons1 = new Button(this);
+            bamSongButtons1.setText(bamSongs.get(index));
+            bamSongsActivity.addView(bamSongButtons1);
 
 
+            index++;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
 
     }
 
 
-    public void keepsmiling (View view) {
-        Intent intent = new Intent(Bars_And_Melody.this, song_lyric_activity.class);
-        startActivity(intent);}
 }
