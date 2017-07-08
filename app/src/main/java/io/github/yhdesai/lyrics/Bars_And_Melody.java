42a47e8dc5281fb009a0af936c022048429dd0e1
Yash <yhdelectric@gmail.com>
2017-07-08 15:51:23 +0530

    The Array Game

--- app/src/main/java/io/github/yhdesai/lyrics/Bars_And_Melody.java
+++ app/src/main/java/io/github/yhdesai/lyrics/Bars_And_Melody.java
@@ -5,10 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-
-
-
+import java.util.ArrayList;
 
 
 public class Bars_And_Melody extends AppCompatActivity {
@@ -18,16 +19,84 @@ public class Bars_And_Melody extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bars__and__melody);
 
-        String [] bamSongs = new String[5];
-
-        bamSongs[0] ="7 years";
-        bamSongs[1] = "Battle Scars";
-        bamSongs[2] = "Beautiful";
-        bamSongs[3] = "Closer";
-        bamSongs[4] = "Dont let me down";
+        ArrayList<String> bamSongs = new ArrayList<>();
 
 
-        Log.e("Bars_And_Melody","Word at index 0: " + bamSongs[0]);
+        bamSongs.add("1");
+
+
+        LinearLayout bamSongsActivity=(LinearLayout)findViewById(R.id.bamSongsActivity);
+
+        int index = 0;
+
+        Button bamSongButtons1 = new Button(this);
+        bamSongButtons1.setText(bamSongs.get(index));
+        bamSongsActivity.addView(bamSongButtons1);
+
+        index = index + 1;
+
+        Button bamSongButtons2 = new Button(this);
+        bamSongButtons2.setText(bamSongs.get(index));
+        bamSongsActivity.addView(bamSongButtons2);
+
+        index = index + 1;
+
+        Button bamSongButtons3 = new Button(this);
+        bamSongButtons3.setText(bamSongs.get(index));
+        bamSongsActivity.addView(bamSongButtons3);
+
+        index = index + 1;
+
+        Button bamSongButtons4 = new Button(this);
+        bamSongButtons4.setText(bamSongs.get(index));
+        bamSongsActivity.addView(bamSongButtons4);
+
+        index = index + 1;
+
+        Button bamSongButtons5 = new Button(this);
+        bamSongButtons5.setText(bamSongs.get(index));
+        bamSongsActivity.addView(bamSongButtons5);
+
+        index = index + 1;
+
+        Button bamSongButtons6 = new Button(this);
+        bamSongButtons6.setText(bamSongs.get(index));
+        bamSongsActivity.addView(bamSongButtons6);
+
+        index = index + 1;
+
+        Button bamSongButtons7 = new Button(this);
+        bamSongButtons7.setText(bamSongs.get(index));
+        bamSongsActivity.addView(bamSongButtons7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
